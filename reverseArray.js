b35e5fb65d1f38e6655a45b7983306125c06e544
unknown <bakewelljake@gmail.com>
2021-06-22 09:23:06 -0500

    reverse array

---
+++ reverseArray.js
@@ -0,0 +1,9 @@
+function reverseArray(arr) {
+    for (let i = 0; i < Math.floor(arr.length/2); i++) {
+        [arr[i], arr[arr.length - 1 - i]] = [arr[arr.length - 1 - i], arr[i]];
+    }
+    console.log(arr);
+    return arr;
+}
+
+reverseArray([1,2,3,4,5,6]);
